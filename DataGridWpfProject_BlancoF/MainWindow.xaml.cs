81f842d45a3258acee621787d6b8bfc0baecd3cb
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-11-16 12:42:32 -0700

    updated stuff

--- DataGridWpfProject_BlancoF/MainWindow.xaml.cs
+++ DataGridWpfProject_BlancoF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DataGridWpfProject_BlancoF.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,40 @@ namespace DataGridWpfProject_BlancoF
     /// </summary>
     public partial class MainWindow : Window
     {
+        // ObservableCollections update its contents better in WPF than Lists
+        ObservableCollection<Restaurant> restaurants;
         public MainWindow()
         {
             InitializeComponent();
+
+            restaurants = new ObservableCollection<Restaurant>()
+            {
+                new Restaurant { Name = "McDonalds", Type = RestaurantType.AMERICAN, Phone = "(801) 555-5555",
+                    Address = "Main St", Rating = 5, Open = true },
+                new Restaurant { Name = "Carls Jr", Type = RestaurantType.AMERICAN, Phone = "(801) 444-4444",
+                    Address = "Center St", Rating = 3, Open = true },
+                new Restaurant { Name = "Pizza Hut", Type = RestaurantType.ITALIAN, Phone = "(801) 333-3333",
+                    Address = "Apple St", Rating = 2, Open = false},
+
+            };
+
+            //RestaurantDataGrid.ItemsSource = restaurants;
+            //DataContext = restaurants;
+            MainPanel.DataContext = restaurants;
+
+        }
+
+        private void AddRestaurant_Click(object sender, RoutedEventArgs e)
+        {
+            Restaurant restaurant = new Restaurant() {
+                Name = "Default",
+                Type = RestaurantType.UNKNOWN,
+                Phone = "000-000-0000",
+                Address = "1st street",
+                Rating = 0,
+                Open = false
+            };
+            restaurants.Add(restaurant);
         }
     }
 }
---
+++ DataGridWpfProject_BlancoF/Models/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridWpfProject_BlancoF.Models
+{
+
+    public enum RestaurantType
+    {
+        AMERICAN,
+        MEXICAN,
+        ITALIAN,
+        UNKNOWN
+    }
+
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public RestaurantType Type { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int Rating { get; set; }
+        public bool Open { get; set; }
+    }
+}
